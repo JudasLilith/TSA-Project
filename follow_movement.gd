737d309c09b298c143fcf3efe737e2d424106182
Myeongjae Seo <peterseo1004@icloud.com>
2025-12-04 19:44:45 -0600

    Introduction
    Yeh just finished, uhhh its a default template from the godot tutorial

---
+++ follow_movement.gd
@@ -0,0 +1,12 @@
+extends Node
+@onready var parent:CharacterBody2D = get_parent()
+
+var start_position
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
