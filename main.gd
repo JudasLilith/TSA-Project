eeeb4ba6c2caedf2f2a1f0768f4a1577a6ef7bc0
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-01-22 23:35:51 -0600

    Trying to get mobs to follow player (plus minor UI adjustments)

--- main.gd
+++ main.gd
@@ -41,19 +41,9 @@ func _on_mob_timer_timeout():
 	var mob_spawn_location = $MobPath/MobSpawnLocation
 	mob_spawn_location.progress_ratio = randf()
 
-	# Set the mob's position to the random location.
-	mob.position = mob_spawn_location.position
-
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
-
-	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
-
-	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
+	# Set the mob's position to the random location, but reset rotation to 0 so it faces forward.
+	mob.global_position = mob_spawn_location.global_position
+	mob.rotation = 0 
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
--- mob.gd
+++ mob.gd
@@ -1,11 +1,14 @@
 extends RigidBody2D
 
+@export var player: CharacterBody2D
+@export var speed: float = 500.0  # Force applied to move
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
 	$AnimatedSprite2D.play()
+	player = get_tree().get_first_node_in_group("player2")
 	
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
@@ -13,3 +16,9 @@ func _ready():
 #
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+func _physics_process(_delta: float) -> void:
+	if player:
+		look_at(player.global_position)
+		var direction = global_position.direction_to(player.global_position)
+		apply_central_force(direction * speed)
